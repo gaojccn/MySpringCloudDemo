0c7cd1e662feb9038c17395e2d29cde59445a828
gaojc <jcgaogs@163.com>
2019-04-10 22:30:56 +0800

    restTemplate

--- zipkin-service-a/src/main/java/com/gaojc/zipkin/service/a/controller/ZipkinController.java
+++ zipkin-service-a/src/main/java/com/gaojc/zipkin/service/a/controller/ZipkinController.java
@@ -2,6 +2,7 @@ package com.gaojc.zipkin.service.a.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,6 +20,11 @@ import org.springframework.web.client.RestTemplate;
 public class ZipkinController {
     @Autowired
     private RestTemplate restTemplate;
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 
     @GetMapping("/hi")
     public String hi() {
