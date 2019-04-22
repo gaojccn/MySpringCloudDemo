f19cd6bb86a338318d269d5d5993900a5bb96688
gaojc <jcgaogs@163.com>
2019-04-23 00:07:32 +0800

    spring-cloud-starter-gateway

---
+++ gateway-server/src/main/java/GetewayServerApplication.java
@@ -0,0 +1,17 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Geteway-server 服务启动类
+ *
+ * @author gaojc
+ * @date 2019/4/22 23:57
+ */
+@SpringBootApplication
+public class GetewayServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GetewayServerApplication.class, args);
+    }
+
+
+}
---
+++ gateway-server/src/main/java/com/gaojc/geteway/server/controller/GetwayServerController.java
@@ -0,0 +1,58 @@
+package com.gaojc.geteway.server.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+/**
+ * @author gaojc
+ * @date 2019/4/23 00:00
+ */
+@RestController
+@Slf4j
+public class GetwayServerController {
+
+    @Bean
+    public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(p -> p
+                        .path("/get")
+                        .filters(f -> f.addRequestHeader("Hello", "World"))
+                        .uri("http://httpbin.org:80"))
+                .build();
+    }
+
+    /**
+     * 加上Hystrix 熔断
+     *
+     * @param builder 创建路由builder
+     * @author gaojc
+     * @date 2019/4/23 0:04
+     */
+    @Bean
+    public RouteLocator myRoutes2(RouteLocatorBuilder builder) {
+        String httpUri = "http://httpbin.org:80";
+        return builder.routes()
+                .route(p -> p
+                        .path("/get")
+                        .filters(f -> f.addRequestHeader("Hello", "World"))
+                        .uri(httpUri))
+                .route(p -> p
+                        .host("*.hystrix.com")
+                        .filters(f -> f
+                                .hystrix(config -> config
+                                        .setName("mycmd")
+                                        .setFallbackUri("forward:/fallback")))
+                        .uri(httpUri))
+                .build();
+    }
+
+    @RequestMapping("/fallback")
+    public Mono<String> fallback() {
+        return Mono.just("fallback");
+    }
+}
